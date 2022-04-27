18afcdbf3bdd5bb24451a3814730a3b197724343
AlexMurmelstein <armurm@gmail.com>
2022-04-27 17:02:09 +0300

    292 fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../img/icons.svg';
+import * as fracty from 'fracty';
 
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
@@ -87,19 +88,7 @@ class RecipeView {
     <h2 class="heading--2">Recipe ingredients</h2>
     <ul class="recipe__ingredient-list">
     ${this.#data.ingredients
-      .map(
-        ing =>
-          `<li class="recipe__ingredient">
-      <svg class="recipe__icon">
-        <use href="${icons}#icon-check"></use>
-      </svg>
-      <div class="recipe__quantity">${ing.quantity}</div>
-      <div class="recipe__description">
-        <span class="recipe__unit">${ing.unit}</span>
-        ${ing.description}
-      </div>
-    </li>`
-      )
+      .map(ing => this.#generateMarkupIngredient(ing))
       .join('')}
     </ul>
   </div>
@@ -124,6 +113,21 @@ class RecipeView {
       </svg>
     </a>
   </div>`;
+  }
+
+  #generateMarkupIngredient(ing) {
+    return `<li class="recipe__ingredient">
+    <svg class="recipe__icon">
+      <use href="${icons}#icon-check"></use>
+    </svg>
+    <div class="recipe__quantity">${
+      ing.quantity ? fracty(ing.quantity) : ''
+    }</div>
+    <div class="recipe__description">
+      <span class="recipe__unit">${ing.unit}</span>
+      ${ing.description}
+    </div>
+  </li>`;
   }
 }
 
