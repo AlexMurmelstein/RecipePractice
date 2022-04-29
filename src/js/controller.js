015c3bde4846d87c46390e1789f418aad11f08de
AlexMurmelstein <armurm@gmail.com>
2022-04-29 13:55:16 +0300

    299 18:45 done html, no eventlisteners

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
@@ -41,7 +42,9 @@ const controlSearchResults = async function () {
     //Load search results
     await model.loadSearchResults(query);
     //Render results
-    resultsView.render(model.getSearchResultsPage());
+    resultsView.render(model.getSearchResultsPage(6));
+    //Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
--- src/js/model.js
+++ src/js/model.js
@@ -7,6 +7,7 @@ export const state = {
     query: '',
     results: [],
     page: 1,
+    resultsPerPage: RESULTS_PER_PAGE,
   },
 };
 
@@ -43,7 +44,7 @@ export const loadSearchResults = async function (query) {
 
 export const getSearchResultsPage = function (page = state.search.page) {
   state.search.page = page;
-  const start = (page - 1) * RESULTS_PER_PAGE;
-  const end = page * RESULTS_PER_PAGE;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
   return state.search.results.slice(start, end);
 };
